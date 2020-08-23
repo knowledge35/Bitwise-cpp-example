eaaab622bb5cc121c767a85814e16c6583f49738
knowledge35 <ibomyakmc@hotmail.com>
2020-08-23 11:37:30 +0300

    merhaba dunya

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	
+	
+	int a=10,x,z;
+  cout<<a<<endl;
+  
+	a<<=2;
+  cout<<a<<endl;
+  
+  a>>=3;
+  
+  cout<<a<<endl;
+ getch();
+    return 0;
+	
+}
